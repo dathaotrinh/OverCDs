d156fb9eb576cd96261f4ee129f54cd640519f6d
Thao Trinh <admin@Thaos-MacBook-Pro.local>
2020-05-21 19:58:23 -0400

    inject HomeService to HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../shared/home.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private homeService: HomeService) { }
 
   ngOnInit(): void {
   }
