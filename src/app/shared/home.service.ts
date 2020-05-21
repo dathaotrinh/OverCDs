4aec6bbb8318fb57cc21119be54406ae101c22c1
Thao Trinh <admin@Thaos-MacBook-Pro.local>
2020-05-21 19:56:43 -0400

    get a list of top pop albums from last.fm

--- src/app/shared/home.service.ts
+++ src/app/shared/home.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class HomeService {
+export class HomeService implements OnInit {
+
+  private apiKey = "143579767c21ff50b45aea9f80b8c1a2";
+  private url = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=pop&api_key=" + this.apiKey + "&format=json";
 
   constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get(this.url)
+      .pipe(
+        map(res => res['albums']),
+        map(res => res['album'])
+      )
+      .subscribe(res => console.log(res));
+  }
 }
