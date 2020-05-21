290b7fc00063f0c648c6be8e17aafbf8dcc4fe03
Thao Trinh <admin@Thaos-MacBook-Pro.local>
2020-05-20 23:27:56 -0400

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const path = require('path');
+const app = express(); app.use(express.static(__dirname + '/dist/overcds')); app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+ '/dist/overcds/index.html'));});
+app.listen(process.env.PORT || 8080);
