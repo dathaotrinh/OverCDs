8b2db2aa462e9028dc60cbbc340e92d9c735eb74
Thao Trinh <admin@Thaos-MacBook-Pro.local>
2020-05-29 02:55:52 -0400

    redesign cart

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Auth } from './auth';
+import { catchError , tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiKey = 'AIzaSyAaz4kQMCaZZw0GnFNJQ7dPk9GAS9zvoOA';
+
+  private signupLink = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
+
+  constructor(private http: HttpClient) { }
+
+  // signupNewUser(email: string, password: string) {
+  //   this.http.post<Auth>(this.signupLink + this.apiKey, {
+  //     email: email,
+  //     password: password,
+  //     returnSecureToken: true
+  //   }).pipe(
+  //     catchError(error => this.handleError(error))
+  //   )
+  // }
+
+  // private handleError(error: HttpErrorResponse){
+  //   let errorMessage = 'An unknown error!';
+    
+  // }
 }
