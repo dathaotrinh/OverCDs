76a42487d01102fb362a25830c722a4536f15f53
Thao Trinh <admin@thaos-mbp.myfiosgateway.com>
2020-06-08 03:01:58 -0400

    clear localStorage when first visit the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'overcds';
+  ngOnInit() {
+    if(localStorage.length === 1) localStorage.clear();
+  }
 }
